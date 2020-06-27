2e425535dca59a6c38018ecf3111e1d6b9281b81
Min Lee <min.navy.lee@gmail.com>
2020-06-26 20:24:41 -0400

    skeletons

---
+++ src/firebase/config.js
@@ -0,0 +1,19 @@
+import * as firebase from "firebase";
+import "@firebase/auth";
+import "@firebase/firestore";
+
+var firebaseConfig = {
+  apiKey: "AIzaSyBqq1S_QqTAYHfAwOzzpkDw2LWyMJh3NWo",
+  authDomain: "parkbreezy-806aa.firebaseapp.com",
+  databaseURL: "https://parkbreezy-806aa.firebaseio.com",
+  projectId: "parkbreezy-806aa",
+  storageBucket: "parkbreezy-806aa.appspot.com",
+  messagingSenderId: "333885035545",
+  appId: "1:333885035545:web:9cbfcd9331d8520e278688",
+  measurementId: "G-DCRM8R6Y5H",
+};
+
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
+export { firebase };
